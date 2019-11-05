557b0a431e998bd95e02771d884fa97a56dad8df
Okal Otieno <okal@justokal.com>
2019-11-05 19:58:11 +0300

    Add DB container and custom network

--- main.tf
+++ main.tf
@@ -1,6 +1,19 @@
 provider "docker" {
 }
 
+resource "docker_container" "db" {
+	image = "${docker_image.db.latest}"
+	name = "db"
+	ports {
+		internal = 5432
+		external = 5432
+	}
+
+	networks_advanced {
+		name = "${docker_network.whisky.name}"
+	}
+}
+
 resource "docker_container" "whisky" {
 	image = "${docker_image.whisky.latest}"
 	name = "app"
@@ -8,8 +21,20 @@ resource "docker_container" "whisky" {
 		internal = 9090
 		external = 9090
 	}
+
+	networks_advanced {
+		name = "${docker_network.whisky.name}"
+	}
 }
 
 resource "docker_image" "whisky" {
 	name = "okal/whisky:latest"
+}
+
+resource "docker_image" "db" {
+	name = "postgres:12.0-alpine"
+}
+
+resource "docker_network" "whisky" {
+	name = "whisky"
 }
